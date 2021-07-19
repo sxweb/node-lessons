4b079039a75103ff66740621b8487410fa41081c
Yuriy <phpred@yandex.ru>
2021-07-19 20:49:37 +0300

    filr system lesson

---
+++ file-system-lesson.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+/*
+fs.mkdir(path.join(__dirname, 'notes'), (error) =>{
+    if(error){
+        throw  new Error(error)
+    }
+    console.log('folder created');
+});
+fs.writeFile(path.join(__dirname, 'notes', 'first-note.txt'),
+    'some simple note',
+    (err) => {
+        if(err) throw new Error(err);
+        console.log('file created');
+    })*/
+/*fs.readFile(
+    path.join(__dirname, 'notes', 'first-note.txt'),
+    (err, data) => {
+        if(err) throw err;
+        console.log(data.toString());
+    }
+)*/
+/*fs.rename(
+    path.join(__dirname, 'notes', 'first-note.txt'),
+    path.join(__dirname, 'notes', 'changed-file.txt'),
+    (err) => {
+        if(err) throw err;
+        console.log('the file renamed');
+    }
+)*/
