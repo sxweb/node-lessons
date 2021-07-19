80ac8e0251337feccce96b7e9cc80bc4dd2630b0
Yuriy <phpred@yandex.ru>
2021-07-19 19:54:25 +0300

    path module lesson

---
+++ path-lesson.js
@@ -0,0 +1,8 @@
+const path = require('path');
+console.log(path.basename(__filename));
+console.log(path.dirname(__filename));
+console.log(path.extname(__filename));
+console.log(path.parse(__filename).ext);
+
+console.log(path.join(__dirname, '/second.html'));
+console.log(path.resolve(__dirname, 'second.html'));
