210b1e11800ba903f6ce6c476843bfbcd9e4df9e
Yuriy <phpred@yandex.ru>
2021-07-21 10:50:52 +0300

    simple server with pages

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer((req, res) =>{
     if(req.method === 'GET'){
         res.writeHead(200, {
-            'Content-Type': 'text/html'
+            'Content-Type': 'text/html; charset=utf-8'
         })
-        res.end(`
-            <h1>Form</h1>
-            <form method="post" action="/">
-                <input name = "title" type="text">
-                <button type="submit">Send</button>
-            </form>
-        `)
+        switch (req.url){
+            case '/':
+                render(req, res, 'index.html');
+                break;
+            case '/about':
+                render(req, res, 'about.html');
+        }
     }else if(req.method === 'POST'){
         const body = [];
         res.writeHead(200, {
@@ -34,3 +36,14 @@ const server = http.createServer((req, res) =>{
 server.listen(3000, () => {
     console.log('Server is running...');
 })
+
+function render(req, res, fileName){
+    fs.readFile(
+        path.join(__dirname, 'views', fileName),
+        'utf-8',
+        (err, content) =>{
+            if(err) throw err;
+            res.end(content);
+        }
+    )
+}
